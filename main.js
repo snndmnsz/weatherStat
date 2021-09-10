c76e4b5ed1811dbed39c6d2e5faedb40fdc9c7c8
snndmnsz <snndmnsz98@gmail.com>
2021-09-11 00:07:09 +0300

    rain?

--- main.js
+++ main.js
@@ -41,6 +41,11 @@ error.style.display = "none";
 
 const error_message = document.querySelector(".error-message");
 
+
+// const cloud_background = document.querySelector("#background-wrap");
+// cloud_background.style.display = 'none';
+
+
 const getCardData = (
   card_icon,
   card_condition,
@@ -291,3 +296,68 @@ const getFlag = async function (name) {
 // wind_dir: "N"
 // wind_kph: 22
 // `;
+
+
+
+var makeItRain = function () {
+  //clear out everything
+  $(".rain").empty();
+
+  var increment = 0;
+  var drops = "";
+  var backDrops = "";
+
+  while (increment < 100) {
+    //couple random numbers to use for various randomizations
+    //random number between 98 and 1
+    var randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
+    //random number between 5 and 2
+    var randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
+    //increment
+    increment += randoFiver;
+    //add in a new raindrop with various randomizations to certain CSS properties
+    drops +=
+      '<div class="drop" style="left: ' +
+      increment +
+      "%; bottom: " +
+      (randoFiver + randoFiver - 1 + 100) +
+      "%; animation-delay: 0." +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"><div class="stem" style="animation-delay: 0.' +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"></div><div class="splat" style="animation-delay: 0.' +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"></div></div>';
+    backDrops +=
+      '<div class="drop" style="right: ' +
+      increment +
+      "%; bottom: " +
+      (randoFiver + randoFiver - 1 + 100) +
+      "%; animation-delay: 0." +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"><div class="stem" style="animation-delay: 0.' +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"></div><div class="splat" style="animation-delay: 0.' +
+      randoHundo +
+      "s; animation-duration: 0.5" +
+      randoHundo +
+      's;"></div></div>';
+  }
+
+  $(".rain.front-row").append(drops);
+  $(".rain.back-row").append(backDrops);
+};
+
+
+
+makeItRain();
